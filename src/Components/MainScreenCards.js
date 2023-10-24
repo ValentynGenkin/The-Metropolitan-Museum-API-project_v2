cfd1de67bded7d0c2666107bf5fda6ac47679fc8
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-10-24 12:01:53 +0200

    main page updated

--- src/Components/MainScreenCards.js
+++ src/Components/MainScreenCards.js
@@ -1,63 +1,36 @@
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import '../CSS/MainScreenCards.css';
 
 function MainScreenCards() {
   return (
-    <Container style={{ padding: '0' }}>
-      <Card
-        style={{
-          marginTop: '10px',
-        }}
-      >
-        <div
-          style={{
-            width: '100%',
-            height: '15rem',
-            display: 'flex',
-          }}
-        >
-          <div style={{ width: '50%' }}>
-            <Card.Title>Arms and Armor</Card.Title>
+    <Container className="main-screen-cards-container">
+      <Card className="card">
+        <div className="main-screen-card">
+          <div className="main-screen-card-information-block">
+            <Card.Title>European Paintings</Card.Title>
             <Card.Body>
               <Card.Text>
-                The principal goals of the Arms and Armor Department are to
-                collect, preserve, research, publish, and exhibit distinguished
-                examples representing the art of the armorer, swordsmith, and
-                gunmaker.
+                The Met's celebrated European Paintings collection encompasses
+                more than 2,500 works of art from the thirteenth through the
+                early twentieth century.
               </Card.Text>
               <Button variant="primary">Explore</Button>
             </Card.Body>
           </div>
-          <div style={{ width: '50%' }}>
+          <div className="main-screen-card-img-block">
             <Card.Img
               variant="top"
-              src="holder.js/100px180"
-              style={{ width: '100%', height: '100%' }}
+              src="assets/main-image1.jpg"
+              className="main-screen-card-img"
             />
           </div>
         </div>
       </Card>
-      <Card
-        style={{
-          marginTop: '10px',
-        }}
-      >
-        <div
-          style={{
-            width: '100%',
-            height: '15rem',
-            display: 'flex',
-          }}
-        >
-          <div style={{ width: '50%' }}>
-            <Card.Img
-              variant="top"
-              src="holder.js/100px180"
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
-          <div style={{ width: '50%' }}>
+      <Card className="card">
+        <div className="main-screen-card left-direction">
+          <div className="main-screen-card-information-block">
             <Card.Title>Arms and Armor</Card.Title>
             <Card.Body>
               <Card.Text>
@@ -68,38 +41,35 @@ function MainScreenCards() {
               </Card.Text>
               <Button variant="primary">Explore</Button>
             </Card.Body>
+          </div>
+          <div className="main-screen-card-img-block">
+            <Card.Img
+              variant="top"
+              src="assets/main-image2.jpg"
+              className="main-screen-card-img"
+            />
           </div>
         </div>
       </Card>
-      <Card
-        style={{
-          marginTop: '10px',
-        }}
-      >
-        <div
-          style={{
-            width: '100%',
-            height: '15rem',
-            display: 'flex',
-          }}
-        >
-          <div style={{ width: '50%' }}>
-            <Card.Title>Arms and Armor</Card.Title>
+      <Card className="card">
+        <div className="main-screen-card">
+          <div className="main-screen-card-information-block">
+            <Card.Title>American Decorative Arts</Card.Title>
             <Card.Body>
               <Card.Text>
-                The principal goals of the Arms and Armor Department are to
-                collect, preserve, research, publish, and exhibit distinguished
-                examples representing the art of the armorer, swordsmith, and
-                gunmaker.
+                Today, the American Wing's ever-evolving collection comprises
+                some 20,000 works by African American, Euro American, Native
+                American, and Latin American artists, ranging from the colonial
+                to early-modern period.
               </Card.Text>
               <Button variant="primary">Explore</Button>
             </Card.Body>
           </div>
-          <div style={{ width: '50%' }}>
+          <div className="main-screen-card-img-block">
             <Card.Img
               variant="top"
-              src="holder.js/100px180"
-              style={{ width: '100%', height: '100%' }}
+              src="assets/main-image3.jpg"
+              className="main-screen-card-img"
             />
           </div>
         </div>
--- src/Components/MainScreenComponent.js
+++ src/Components/MainScreenComponent.js
@@ -6,6 +6,7 @@ function MainScreenComponent() {
   return (
     <>
       <Carousel />
+
       <MainScreenWelcomeImg />
 
       <MainScreenCards />
